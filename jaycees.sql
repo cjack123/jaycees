802f573139c1c210d94cbb06686b7d455f3e3c85
Chaneice Jackson <c.jackson0126@gmail.com>
2022-08-27 09:20:01 -0500

    fictures complete

---
+++ jaycees.sql
@@ -0,0 +1,17 @@
+SELECT * 
+FROM jayceesapi_flavor;
+
+SELECT * 
+FROM jayceesapi_size;
+
+SELECT * 
+FROM jayceesapi_toppings;
+
+SELECT * 
+FROM jayceesapi_order;
+
+SELECT * 
+FROM auth_user;
+
+SELECT * FROM 
+authtoken_token;
